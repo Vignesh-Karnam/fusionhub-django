e0eff22a9b07d7d9d6c2f82350061eb580ef6ad6
Vignesh-Karnam <vigneshkarnam4111@gmail.com>
2025-09-08 23:24:35 +0530

    added login form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,13 +1,41 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(
-        widget=forms.EmailInput(attrs={"class": "input-field", "placeholder": "Email"})
+        label=_('Email'),
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5', 
+                'placeholder': 'name@company.com' 
+            }
+        ),
+        required=True
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "input-field", "placeholder": "Password"})
+        label=_('Password'),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'current-password',
+                'class': 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5',
+                'placeholder': '••••••••'
+            }
+        ),
+        required= True
     )
+    remember_me = forms.CharField(
+        label=_('Remember me'),
+        widget=forms.CheckboxInput(
+            attrs={
+                'class': 'w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300'
+            }
+        ),
+        required=True
+    )
+
 
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,36 +1,8 @@
-from django.contrib.auth.views import (LoginView, LogoutView,
-                                       PasswordResetCompleteView,
-                                       PasswordResetConfirmView,
-                                       PasswordResetDoneView,
-                                       PasswordResetView)
-from django.urls import reverse_lazy
-
+from django.contrib.auth.views import LoginView
 from .forms import CustomAuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class CustomLoginView(LoginView):
-    template_name = "users/login.html"
-    authentication_form = CustomAuthenticationForm
-
-
-class CustomLogoutView(LogoutView):
-    next_page = reverse_lazy("login")
-
-
-class CustomPasswordResetView(PasswordResetView):
-    template_name = "users/password_reset.html"
-    email_template_name = "users/password_reset_email.html"
-    success_url = reverse_lazy("password_reset_done")
-
-
-class CustomPasswordResetDoneView(PasswordResetDoneView):
-    template_name = "users/password_reset_done.html"
-
-
-class CustomPasswordResetConfirmView(PasswordResetConfirmView):
-    template_name = "users/password_reset_confirm.html"
-    success_url = reverse_lazy("password_reset_complete")
-
-
-class CustomPasswordResetCompleteView(PasswordResetCompleteView):
-    template_name = "users/password_reset_complete.html"
+    template_name = 'users/login.html'
+    form_class = CustomAuthenticationForm
