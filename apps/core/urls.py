9022ab69476f227820c07f6ed98cff23858032c8
Vignesh-Karnam <vigneshkarnam4111@gmail.com>
2025-09-17 22:19:21 +0530

    added pricing page

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('pricing/', views.pricing, name='pricing')
 ]
