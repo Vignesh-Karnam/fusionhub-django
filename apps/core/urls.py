327161c28bc37c8797773947d46adf47143d19f9
Vignesh-Karnam <vigneshkarnam4111@gmail.com>
2025-09-17 22:58:36 +0530

    added functionality in navbar

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('pricing/', views.pricing, name='pricing')
+    path('pricing/', views.pricing, name='pricing'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about')
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -5,3 +5,12 @@ def home(request):
 
 def pricing(request):
     return render(request, 'core/pricing.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+
+def about(request):
+    return render(request, 'core/about.html')
