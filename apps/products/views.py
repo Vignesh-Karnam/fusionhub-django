6346de9aee792b8e15bd1dbb7b30bf3f60b5e28f
Vignesh-Karnam <vigneshkarnam4111@gmail.com>
2025-09-24 19:03:43 +0530

    updated tailwind

--- apps/products/views.py
+++ apps/products/views.py
@@ -17,7 +17,7 @@ class ProductsView(LoginRequiredMixin, ListView):
     template_name = 'products/products.html'
     model = Product
     context_object_name = 'products'
-    paginate_by = 10
+    paginate_by = 7
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
